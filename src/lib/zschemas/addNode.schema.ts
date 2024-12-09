61cd1337d5a909f55bef3d1677d5643e2aa6b464
Mathis <mathisjung02@gmail.com>
2024-12-10 00:37:55 +0100

    fix trad bug

--- src/lib/zschemas/addNode.schema.ts
+++ src/lib/zschemas/addNode.schema.ts
@@ -3,10 +3,10 @@ import { t } from 'svelte-i18n';
 import { z } from 'zod';
 
 export const addNodeSchema = z.object({
-	title: z.string({ message: get(t)('errors.missingTitle') }),
-	text: z.string({ message: get(t)('errors.missingText') }),
-	author: z.string({ message: get(t)('errors.missingAuthor') }),
-	parent: z.string({ message: get(t)('errors.addNode.missingParent') }),
-	session: z.string({ message: get(t)('errors.addNode.missingSession') }),
-	side: z.string({ message: get(t)('errors.addNode.missingSide') })
+	title: z.string({ message: 'errors.missingTitle' }),
+	text: z.string({ message: 'errors.missingText' }),
+	author: z.string({ message: 'errors.missingAuthor' }),
+	parent: z.string({ message: 'errors.addNode.missingParent' }),
+	session: z.string({ message: 'errors.addNode.missingSession' }),
+	side: z.string({ message: 'errors.addNode.missingSide' })
 });
