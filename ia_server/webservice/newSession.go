b44903767c3b3a9c77728a45f1c41ab3541bad01
EileenLZ <74616972+EileenLZ@users.noreply.github.com>
2025-01-08 03:22:23 +0100

    un-revert commit

--- ia_server/webservice/newSession.go
+++ ia_server/webservice/newSession.go
@@ -35,20 +35,13 @@ func (rsa *ServerAgent) DoNewSession(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-<<<<<<< HEAD
 	new_session, err := censorship.NewSession(req, libs.ScenarioFile)
-=======
-	new_session, err := NewSession(req.Session, scenarioFile)
-	rsa.Sessions[req.Session] = *new_session
-
->>>>>>> parent of 15af0df (IA server)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, err.Error())
 		return
 	}
 
-<<<<<<< HEAD
 	rsa.Sessions[req.SessionID] = new_session
 
 	//sauvegarder la nouvelle sessions
@@ -56,10 +49,6 @@ func (rsa *ServerAgent) DoNewSession(w http.ResponseWriter, r *http.Request) {
 
 	var resp libs.NewSessionResponse
 	resp.Session = req.SessionID
-=======
-	var resp NewSessionResponse
-	resp.Session = req.Session
->>>>>>> parent of 15af0df (IA server)
 
 	w.WriteHeader(http.StatusOK)
 	serial, _ := json.Marshal(resp)
