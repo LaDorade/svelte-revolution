a3b1fb9c5bebc31280e4a1342168945ba915a700
Mathis <mathisjung02@gmail.com>
2025-01-17 20:43:39 +0100

    fix debug pane auth

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,9 @@ import { waitLocale } from 'svelte-i18n';
 import { pb } from '$lib/client/pocketbase';
 import type { LayoutLoad } from './$types';
 
+export const ssr = false;
+export const prerender = false;
+
 export const load: LayoutLoad = async () => {
 	await waitLocale();
 	if (pb.authStore.isValid && pb.authStore.record) {
