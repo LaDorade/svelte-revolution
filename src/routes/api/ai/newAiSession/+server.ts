4b022d73728678c63ae2e961d6dbd26a3bbd770e
Mathis <mathisjung02@gmail.com>
2025-01-21 09:40:51 +0100

    log errors

--- src/routes/api/ai/newAiSession/+server.ts
+++ src/routes/api/ai/newAiSession/+server.ts
@@ -10,35 +10,40 @@ export const POST: RequestHandler = async ({ request }) => {
 		return json({ ok: false, message: 'IA server not reachable' });
 	}
 
-	const { sessionId, scenarioId, cookies } = await request.json();
+	try {
+		const { sessionId, scenarioId, cookies } = await request.json();
 
-	const pb = new PocketBase(DB_URL);
-	pb.authStore.loadFromCookie(cookies);
-	if (!pb.authStore.isValid) {
-		return json({ ok: false, message: 'Invalid credentials' });
-	}
+		const pb = new PocketBase(DB_URL);
+		pb.authStore.loadFromCookie(cookies);
+		if (!pb.authStore.isValid) {
+			return json({ ok: false, message: 'Invalid credentials' });
+		}
 
-	const sides = await pb
-		.collection('side')
-		.getFullList({ filter: pb.filter('scenario = {:scenario}', { scenario: scenarioId }) });
-	const ends = await pb
-		.collection('end')
-		.getFullList({ filter: pb.filter('scenario = {:scenario}', { scenario: scenarioId }) });
+		const sides = await pb
+			.collection('side')
+			.getFullList({ filter: pb.filter('scenario = {:scenario}', { scenario: scenarioId }) });
+		const ends = await pb
+			.collection('end')
+			.getFullList({ filter: pb.filter('scenario = {:scenario}', { scenario: scenarioId }) });
 
-	const response = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			session: sessionId,
-			sides: sides.map((side) => {
-				return { id: side.id, name: side.name };
-			}),
-			ends: ends.map((end) => {
-				return { id: end.id, title: end.title };
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				session: sessionId,
+				sides: sides.map((side) => {
+					return { id: side.id, name: side.name };
+				}),
+				ends: ends.map((end) => {
+					return { id: end.id, title: end.title };
+				})
 			})
-		})
-	});
-	return json(response.ok);
+		});
+		return json(response.ok);
+	} catch (e) {
+		console.error(e);
+		return json({ ok: false, message: e.message });
+	}
 };
