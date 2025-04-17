9c461797cee9ff9968cddbd06d1db6be21cf8097
ericb <ericbmousy@gmail.com>
2025-04-17 23:34:48 +0200

    reset du fichier main car sert à rien

--- ia_mistral/main.py
+++ ia_mistral/main.py
@@ -1,25 +1,14 @@
-""" Exemple de Chat GPT
-    Regarde un peu comment ça fonctionne """
-
 from pocketbase import PocketBase
 import os
-from fastapi import FastAPI
-from session_watcher import get_ai_sessions
 
-app = FastAPI()
 client = PocketBase("https://db.babel-revolution.fr")
 PB_EMAIL = os.getenv("POCKETBASE_EMAIL")
 PB_PASSWORD = os.getenv("POCKETBASE_PASSWORD")
 
-@app.on_event("startup")
-async def startup_event():
-    print("🚀 Lancement du watcher de sessions")
+def main():
+    pass
 
-@app.get("/sessions_ia_actives")
-def get_active_ai_sessions():
-    sessions_avec_ia = get_ai_sessions(client)
 
-    # il reste à filtrer les sessions obtenues pour ne garder que celles qui sont actives
-    # question: comment déterminer si une session est active ?
-
-    return sessions_avec_ia
+if __name__ == "__main__":
+    main()
+    
