345d3231552af602feb15ccf8d2f65d9524310c0
Eric Bjarstal <ericbmousy@gmail.com>
2025-06-14 20:35:59 +0200

    add fichier de test connexion pocketbase et requete sur table TriggerNodes

---
+++ ia_mistral/test_pb.py
@@ -0,0 +1,39 @@
+from pocketbase import PocketBase
+import os
+from dotenv import load_dotenv
+
+"""
+Structure d'un TriggerNode dans la BDD:
+id: int
+scenario: int
+infos: json
+{
+  "id":
+  "conditions":
+  "triggers": [],
+  "title":
+  "text":
+  "author":
+}
+created: date
+updated: date
+"""
+
+if __name__ == "__main__":
+    try:
+        load_dotenv()
+        PB_LOGIN = os.getenv("PB_LOGIN")
+        PB_PASSWORD = os.getenv("PB_PASSWORD")
+    except Exception as e:
+        print("❌ Erreur chargement .env:", e)
+
+    try:
+        client = PocketBase("https://db.babel-revolution.fr")
+        admin_data = client.admins.auth_with_password(str(PB_LOGIN), str(PB_PASSWORD))
+        table = client.collection("TriggerNodes").get_list().items
+        for ligne in table:
+            for trigger_node in ligne.nodes:
+                if "conditions" in trigger_node:
+                    print(len(trigger_node["conditions"]))
+    except Exception as e:
+        print("❌ Erreur Pocketbase:", e)
